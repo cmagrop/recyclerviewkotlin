fc5d560e0e29f3de6357d4ad6097b6d5b596a0a4
=Christian Magro Pérez <cmagrop@gmail.com>
2024-06-24 22:46:27 -0400

    Actualizacion 1 Basico Adapter

--- app/src/main/java/com/example/recyclerviewkotlin/BasicoAdapter.kt
+++ app/src/main/java/com/example/recyclerviewkotlin/BasicoAdapter.kt
@@ -1,8 +1,74 @@
 package com.example.recyclerviewkotlin
 
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class BasicoAdapter: RecyclerView.Adapter<BasicoAdapter.ViewHolder> {
 
+    lateinit var listaDatos:ArrayList<String>
 
+    constructor(listaDatos: ArrayList<String>) : super() {
+        this.listaDatos = listaDatos
+    }
+
+
+    /*
+    *
+    *
+    * public class ViewHolder extends RecyclerView.ViewHolder
+    * {
+    * public ViewHolder(View itemLayoutView)
+    * {
+    * super(itemLayoutView);
+    * }
+    *
+    * }
+    *
+    * */
+
+
+    public class ViewHolder(itemView: View):RecyclerView.ViewHolder(itemView)
+    {
+
+        //TextView item;
+        lateinit var item: TextView
+
+        init {
+
+            //item=itemView.findViewById(R.id.itemId);
+
+            item = itemView.findViewById(R.id.itemId)
+
+        }
+
+        fun asignarDatos(itemDato: String)
+        {
+            item.setText(itemDato)
+
+        }
+
+
+
+
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        var view:View = LayoutInflater.from(parent.context).
+        inflate(R.layout.listbasico,null,false)
+        //return new ViewHolder(view);
+        return ViewHolder(view)
+
+    }
+
+    override fun getItemCount(): Int {
+        return listaDatos.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
 }
