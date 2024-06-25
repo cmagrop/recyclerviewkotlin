dc86b8b5327de88647aea4145b8cb3a7be70cc90
=Christian Magro Pérez <cmagrop@gmail.com>
2024-06-24 20:25:56 -0400

    Proyecto inicial RecyclerView y layouts

---
+++ app/src/main/java/com/example/recyclerviewkotlin/BasicoActivity.kt
@@ -0,0 +1,17 @@
+package com.example.recyclerviewkotlin
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class BasicoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_basico)
+        //aqui empieza la acción
+        }
+    }
+}
---
+++ app/src/main/java/com/example/recyclerviewkotlin/BasicoAdapter.kt
@@ -0,0 +1,8 @@
+package com.example.recyclerviewkotlin
+
+import androidx.recyclerview.widget.RecyclerView
+
+class BasicoAdapter: RecyclerView.Adapter<BasicoAdapter.ViewHolder> {
+
+
+}
